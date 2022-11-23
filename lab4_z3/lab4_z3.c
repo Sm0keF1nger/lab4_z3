6f1e86209453c3266a2a3a882be8c4154a2042fa
Sm0keF1nger <kkaptelov@gmail.com>
2022-11-23 22:41:32 +0200

    first commit

---
+++ lab4_z3/lab4_z3.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+
+
+int main()
+{
+	int j , i, x, n, sum = 0, sum2 = 0;
+	
+	printf_s("insert n: ");
+	scanf_s("%d", &n);
+
+	printf_s("insert x: ");
+	scanf_s("%d", &x);
+	
+		
+
+	
+	for (int i = 1; i <= n; i++)
+	{
+		
+		for (j = 2; j <= n; j++)
+		{
+			sum += x + j;
+		}
+
+		
+		
+	} 
+	
+	printf_s("sum = %d\n", sum);
+	
+}
+
+
